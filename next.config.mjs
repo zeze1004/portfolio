14de4c955acc89b5c4cb849370c52a1464fb13f1
sojung.kim <thwjd2717@gmail.com>
2025-05-23 15:09:39 +0900

    chore: 배포 환경 수정

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,8 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
   images: {
     unoptimized: true,
   },
